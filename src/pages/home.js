5640b3846323581feea772bf36271cb3aeabb3c0
williamrr <william.rodriguez@alteru.co>
2021-06-30 20:05:18 -0500

    finalizacion de los estilos del proyecto

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,19 @@
-const Home = () => {
-    const view = `
+import getData from "../utils/getData";
+
+const Home = async() => {
+        const characters = await getData();
+        const view = `
     <div class="Characters">
-        <article class="Character-item">
-            <a href="#/1/">
-                <img src="image" alt="name"/>
-                <h2>Name</h2>
+        ${characters.results.map(character => `
+        <article class="Characters-item">
+            <a href="#/${character.id}/">
+                <img src="${character.image}" alt="${character.name}"/>
+                <h2>${character.name}</h2>
             </a>
         </article> 
+        `).join('')}
+
+        
     </div>
     `;
     return view;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { resolve } = require('path');
+//const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -25,6 +26,12 @@ module.exports = {
             inject: true,
             template: './public/index.html',
             filename: './index.html',
-        })
+        }),
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: './src/styles/styles.css',
+                to: ''
+            }]
+        }),
     ]
 }
