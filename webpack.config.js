5b768983975603a15360727438173fb7a722bd6f
williamrr <william.rodriguez@alteru.co>
2021-07-02 09:37:51 -0500

    se deshacen cambios de los dos ultimos commits

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,37 @@
-//prueba con nuevo archivo webpack.config.co//
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+//const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: "./src/App.js",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
     module: {
         rules: [{
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|jpg|gif)$/i,
-                use: [{
-                    loader: "url-loader",
-                    options: {
-                        limit: 8192,
-                    },
-                }, ],
-            },
-        ],
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            }
+        }]
     },
-
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./public/index.html",
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html',
         }),
-    ],
-};
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: './src/styles/styles.css',
+                to: ''
+            }]
+        }),
+    ]
+}
